22c161c0b461da91088cad72a8e4602cfc1349df
dan <8881965+dcrgll@users.noreply.github.com>
2022-12-04 13:02:14 +0000

    Fix layout issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export default function Page() {
+export default function Page(): JSX.Element {
 	return (
 		<div className="h-screen w-screen flex justify-center items-center">
 			<h1 className="text-gray-800 text-center font-extrabold text-8xl">
